3bc13d747bd96524ec248efc4cd06e50735a9cf2
Alol-sw <36926686+Alol-sw@users.noreply.github.com>
2018-03-24 02:15:24 +0100

    Add files via upload

---
+++ RelationalOperators/src/relationaloperators/RelationalOperators.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package relationaloperators;
+
+/**
+ *
+ * @author Alaa-sw
+ * 24.03.2018
+ */
+public class RelationalOperators {
+
+    
+    public static void main(String[] args) {
+        
+      int a = 10;
+      int b = 20;
+
+      System.out.println("a == b = " + (a == b) );
+      System.out.println("a != b = " + (a != b) );
+      System.out.println("a > b = " + (a > b) );
+      System.out.println("a < b = " + (a < b) );
+      System.out.println("b >= a = " + (b >= a) );
+      System.out.println("b <= a = " + (b <= a) );
+        
+    }
+    
+}
